3e12d52167cef235fb161690494d3f7c06ba75b0
ChenHaoYin <haoyinag0@hotmail.com>
2020-07-20 09:29:23 +0800

    feat

---
+++ four/target/rls/debug/deps/four-0bf033b03929e11e.d
@@ -0,0 +1,5 @@
+d:\Code\Test\Rust\four\target\rls\debug\deps\four-0bf033b03929e11e.rmeta: src\main.rs
+
+d:\Code\Test\Rust\four\target\rls\debug\deps\four-0bf033b03929e11e.d: src\main.rs
+
+src\main.rs:
---
+++ four/target/rls/debug/deps/four-bce44259e49d34bb.d
@@ -0,0 +1,5 @@
+d:\Code\Test\Rust\four\target\rls\debug\deps\four-bce44259e49d34bb.rmeta: src\main.rs
+
+d:\Code\Test\Rust\four\target\rls\debug\deps\four-bce44259e49d34bb.d: src\main.rs
+
+src\main.rs:

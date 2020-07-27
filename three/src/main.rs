b252582e54064e80e883cf6e8fa0f8da4963766c
陈浩银 <chen.haoyin@angsivip.com>
2020-07-27 13:55:03 +0800

    feat：简单类型测验

--- three/src/main.rs
+++ three/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // let mut a = String::from("333");
+    let a = "333"; // 会自动定义类型，相当于 String::from("333");
+                   // a = 123; // 会报错，因为上面已经声明为String
+    println!("a is {}", a);
+
+    let b = 456;
+    println!("b is {0}", b);
+
+    let c = "呵呵哈哈";
+    println!("c is {0}", c)
 }

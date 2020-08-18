2145161ca65e4dea54889e9f00db5537532e3ca8
陈浩银 <chen.haoyin@angsivip.com>
2020-08-18 11:55:42 +0800

    feat：多输出、类型说明

--- three/src/main.rs
+++ three/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
     // let mut a = String::from("333");
-    let a = "333"; // 会自动定义类型，相当于 String::from("333");
+    // Rust 是强类型语言，但具有自动判断变量类型的能力。这很容易让人与弱类型语言产生混淆。
+    let a = "123"; // 会自动定义类型，相当于 String::from("333");
                    // a = 123; // 会报错，因为上面已经声明为String
-    println!("a is {}", a);
+    println!("a is {}, a again is {}", a, a);
 
     let b = 456;
-    println!("b is {0}", b);
+    println!("a is {}, b is {}", a, b);
 
     let c = "呵呵哈哈";
-    println!("c is {0}", c)
+    println!("a is {}, b is {} ,c is {}", a, b, c)
 }
